4c9a8ded7b655152825726593d8f27d7e2c4b665
martinzamora <martin.zamora@mavs.uta.edu>
2017-09-28 14:11:12 -0500

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -20,6 +20,14 @@ public:
 private slots:
     void on_confirmbutton_clicked();
 
+    void on_confirmbutton_2_pressed();
+
+    void on_confirmbutton_3_clicked();
+
+    void on_confirmbutton_4_clicked();
+
+    void on_confirmbutton_5_clicked();
+
 private:
     Ui::MainWindow *ui;
     void writeInputNumbers(int x, int y, int z);
